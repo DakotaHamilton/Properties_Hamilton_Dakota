f4bb25840acd3e12f89e82a91c90b898161b9882
DakotaHamilton <113059394+DakotaHamilton@users.noreply.github.com>
2023-01-18 10:11:07 -0600

    Add project files.

---
+++ Properties_Hamilton_Dakota/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Hamilton_Dakota
+{
+    class Car
+    {
+        // Private string variable
+        private string model;
+        // Protected variable string
+        public string Model { get { return model; } set { model = value; }  }
+        // Automatic Property
+        public string Make { get; set; }
+
+    }
+}
---
+++ Properties_Hamilton_Dakota/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Properties_Hamilton_Dakota
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"> List of Cars and their Make and Model </param>
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            Car car3 = new Car();
+            car3.Make = "Kia";
+            car3.Model = "Sportage";
+
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
+        }
+    }
+}
